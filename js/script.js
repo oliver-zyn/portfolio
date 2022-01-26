052afe242a041ad921a937793381b9f21df48bf0
oliver-zyn <olivermayer15@gmail.com>
2022-01-26 19:30:30 -0300

    Criação da sessão de "Quem sou" e de "Projetos"

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
+
+  function flipCard() {
+      let card = document.querySelector('#card1')
+
+      card.classList.add('flip')
+  }
+  
+  function undflipCard() {
+    let card = document.querySelector('#card1')
+
+    card.classList.remove('flip')
+}
+
+  
